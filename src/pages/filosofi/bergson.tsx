8b000caa789e46bf25b272489cae98cac61345e4
exodia-info <bianchinmauro2.0@gmail.com>
2023-10-25 08:51:26 +0200

    bergson

---
+++ src/pages/filosofi/bergson.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/no-children-prop */
+import React, { useEffect, useRef } from 'react'
+import Layout from '../components/Layout'
+import Head from 'next/head'
+import Indietro from '../components/Indietro'
+import Content from '../components/Content'
+// import Commento from '../components/Commento'
+import BergsonContent from '../descrizioni/bergson.mdx'
+import ImageMagnifier from '../components/ImageMagnifier'
+
+
+const bergson = () => {
+
+    const bergsonUrl = '../assets/bergsonbeffa.jpg'
+
+    return (
+        <Layout children={
+            <>
+                <Head>
+                    <title>Il Pensiero di Bergson in un Disegno</title>
+                    <meta name="description" content="Il Pensiero di Bergson in un Disegno " />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <main >
+                    <div className='flex md:flex-row flex-col items-center justify-center my-20  gap-8 md:h-[105vh]'>
+                    <div  className='flex flex-col md:w-[45%] w-[90%]  md:h-[100%] gap-3'>
+                        <div className=''>
+                            <ImageMagnifier src={bergsonUrl} width='100%' />
+                        </div>
+                        <div className='hidden md:flex flex-col relative'>
+                            <Indietro />
+                        </div>
+                    </div>
+                    <div className='flex flex-col md:w-[45%] w-[90%] md:max-h-[100%] max-h-[75vh] gap-3'>
+                        <div  className=' rounded-lg overflow-x-hidden font-poppins bg-zinc-100 border-2 border-slate-400 p-6 overflow-y-scroll'>
+                            <Content content={<BergsonContent />} />
+                        </div>
+                        {/* <div className='border-red-600 border-2'>
+                            <Commento />
+                        </div> */}
+                        <div className='flex md:hidden flex-col relative'>
+                            <Indietro />
+                        </div>
+                    </div>
+                    </div>
+                </main>
+            </>
+        } />
+    )
+}
+
+export default bergson
