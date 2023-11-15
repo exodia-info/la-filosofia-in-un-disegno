cc2d1bc92c3560eae79dd2afe732a6f912308e88
exodia-info <bianchinmauro2.0@gmail.com>
2023-11-15 12:31:25 +0100

    percorsi pubblici

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 []
 export default authMiddleware({
-  publicRoutes: ['/', "/api/trpc/post.hello", "/filosofi/schopenhauer"]
+  publicRoutes: ['/', "/api/trpc/post.hello", "/filosofi/schopenhauer", "/filosofi/kierkegaard", "/filosofi/feuerbach", "/filosofi/marx", "/filosofi/comte", "/filosofi/bergson", "/filosofi/nietzsche"]
 });
 
 // Stop Middleware running on static files
