8a619f5761e82646b98b54f45a00507279052cac
exodia-info <bianchinmauro2.0@gmail.com>
2024-03-23 22:33:47 +0100

    rrr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import Header from "./components/Header";
 import { Toaster } from "react-hot-toast";
 import { useRouter } from "next/router";
 import "../i18n";
+import { NextUIProvider } from "@nextui-org/react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const router = useRouter();
@@ -18,16 +19,18 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
       <ClerkProvider {...pageProps}>
-        <Head>
-          <title>La Filosofia in un Disegno</title>
-          <meta name="description" content="" />
-          <link rel="icon" href="/exodia.png" />
-        </Head>
-        <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#efeda9] to-[#eae38d]">
-          <Header />
-          <Toaster />
-          <Component {...pageProps} />
-        </main>
+        <NextUIProvider>
+          <Head>
+            <title>La Filosofia in un Disegno</title>
+            <meta name="description" content="" />
+            <link rel="icon" href="/exodia.png" />
+          </Head>
+          <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#efeda9] to-[#eae38d]">
+            <Header />
+            <Toaster />
+            <Component {...pageProps} />
+          </main>
+        </NextUIProvider>
       </ClerkProvider>
     </>
   );
